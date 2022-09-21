57afb736ccbddee1c5a184f9b6fedb7b4de045de
Airam Toscano <airamlobato@gmail.com>
2022-09-21 15:48:22 -0400

    Feat: invetory: json.files

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 
 from ..reports.simple_report import SimpleReport
@@ -8,6 +9,15 @@ from ..reports.complete_report import CompleteReport
 class Inventory():
 
     def import_data(caminho, tipodeRelatorio):
+        caminhoCerto = caminho.split('.')
+        if (caminhoCerto[1] == 'csv'):
+            csv = Inventory.importcsv(caminho, tipodeRelatorio)
+            return csv
+        if (caminhoCerto[1] == 'json'):
+            json = Inventory.importjson(caminho, tipodeRelatorio)
+            return json
+
+    def importcsv(caminho, tipodeRelatorio):
         if (tipodeRelatorio == "simples"):
             lista = []
             with open(caminho) as file:
@@ -22,3 +32,13 @@ class Inventory():
                 for row in read:
                     lista.append(row)
             return CompleteReport.generate(lista)
+
+    def importjson(caminho, tipodeRelatorio):
+        if (tipodeRelatorio == "simples"):
+            with open(caminho) as file:
+                read = json.load(file)
+            return SimpleReport.generate(read)
+        if (tipodeRelatorio == "completo"):
+            with open(caminho) as file:
+                read = json.load(file)
+            return CompleteReport.generate(read)
